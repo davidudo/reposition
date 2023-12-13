7528a9ba43d2c14c7928cd1d22a534b755f431d7
David Udo <udodavid46.ud@gmail.com>
2023-12-14 00:12:27 +0100

    Format code base

--- re_position/wsgi.py
+++ re_position/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 're_position.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "re_position.settings")
 
 application = get_wsgi_application()
