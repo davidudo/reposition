9d4ce497b25d9fe5bc4f01288430ade4d1fe1748
David Udo <udodavid46.ud@gmail.com>
2023-12-14 00:35:14 +0100

    Remove secret key

--- re_position/settings.py
+++ re_position/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j=2&*fw1kpwq1#z6oakp1!2m-gd4r8yd0zmxx#9+$n2jpdqgss'
+SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
