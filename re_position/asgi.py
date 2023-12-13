597edd96d23593490b459b449076c2d162b7434a
David Udo <udodavid46.ud@gmail.com>
2023-12-14 00:12:27 +0100

    Format code base

--- re_position/asgi.py
+++ re_position/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 're_position.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "re_position.settings")
 
 application = get_asgi_application()
