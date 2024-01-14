50c19922a80d101b4cad91f5e1904342056919da
David Udo <udodavid46.ud@gmail.com>
2024-01-14 15:22:51 +0100

    Render product details

--- products/products.py
+++ products/products.py
@@ -1,56 +1,201 @@
 products = [
     {
-        "_id": "1",
+        "id": "1",
         "name": "Cotton Blend T-Shirt",
         "images": [
-            "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-01.jpg",
-            "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-02.jpg",
+            "https://tailwindui.com/img/ecommerce-images/product-page-01-featured-product-shot.jpg",
+            "https://tailwindui.com/img/ecommerce-images/product-page-01-product-shot-01.jpg",
+            "https://tailwindui.com/img/ecommerce-images/product-page-01-product-shot-02.jpg",
         ],
-        "price": 19.99,
+        "colors": ["gray", "black"],
+        "sizes": ["xxs", "xs", "s", "m", "l", "xl"],
+        "description": """
+        The Basic tee is an honest new take on a classic. The tee uses super soft,
+        pre-shrunk cotton for true comfort and a dependable fit. They are hand cut
+        and sewn locally, with a special dye technique that gives each tee it's own
+        look. Looking to stock your closet? The Basic tee also comes in a 3-pack or
+        5-pack at a bundle discount.
+        """,
+        "rating": 4.9,
+        "additional_information": [
+            {
+                "title": "Features",
+                "text": [
+                    "Multiple strap configurations",
+                    "Spacious interior with top zip",
+                    "Leather handle and tabs",
+                    "Interior dividers",
+                    "Stainless strap loops",
+                    "Double stitched construction",
+                    "Water-resistant",
+                ],
+            }
+        ],
+        "price": 79999.99,
     },
     {
-        "_id": "2",
+        "id": "2",
         "name": "Denim Jeans",
         "images": [
-            "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-01.jpg",
-            "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-02.jpg",
+            "https://tailwindui.com/img/ecommerce-images/product-page-01-featured-product-shot.jpg",
+            "https://tailwindui.com/img/ecommerce-images/product-page-01-product-shot-01.jpg",
+            "https://tailwindui.com/img/ecommerce-images/product-page-01-product-shot-02.jpg",
         ],
-        "price": 59.99,
+        "colors": ["gray", "black"],
+        "sizes": ["xs", "s", "m", "l", "xl"],
+        "description": """
+        The Basic tee is an honest new take on a classic. The tee uses super soft,
+        pre-shrunk cotton for true comfort and a dependable fit. They are hand cut and sewn locally,
+        with a special dye technique that gives each tee it's own look. Looking to stock your closet?
+        The Basic tee also comes in a 3-pack or 5-pack at a bundle discount.
+        """,
+        "rating": 3.9,
+        "additional_information": [
+            {
+                "title": "Features",
+                "text": [
+                    "Multiple strap configurations",
+                    "Spacious interior with top zip",
+                    "Leather handle and tabs",
+                    "Interior dividers",
+                    "Stainless strap loops",
+                    "Double stitched construction",
+                    "Water-resistant",
+                ],
+            }
+        ],
+        "price": 59999.99,
     },
     {
-        "_id": "3",
+        "id": "3",
         "name": "Cashmere Sweater",
         "images": [
-            "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-01.jpg",
-            "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-02.jpg",
+            "https://tailwindui.com/img/ecommerce-images/product-page-01-featured-product-shot.jpg",
+            "https://tailwindui.com/img/ecommerce-images/product-page-01-product-shot-01.jpg",
+            "https://tailwindui.com/img/ecommerce-images/product-page-01-product-shot-02.jpg",
         ],
-        "price": 129.99,
+        "colors": ["gray", "black"],
+        "sizes": ["xxs", "xs", "s", "m", "l", "xl"],
+        "description": """
+        The Basic tee is an honest new take on a classic. The tee uses super soft,
+        pre-shrunk cotton for true comfort and a dependable fit. They are hand cut and sewn locally,
+        with a special dye technique that gives each tee it's own look. Looking to stock your closet?
+        The Basic tee also comes in a 3-pack or 5-pack at a bundle discount.
+        """,
+        "rating": 3.9,
+        "additional_information": [
+            {
+                "title": "Features",
+                "text": [
+                    "Multiple strap configurations",
+                    "Spacious interior with top zip",
+                    "Leather handle and tabs",
+                    "Interior dividers",
+                    "Stainless strap loops",
+                    "Double stitched construction",
+                    "Water-resistant",
+                ],
+            }
+        ],
+        "price": 129999.99,
     },
     {
-        "_id": "4",
+        "id": "4",
         "name": "Leather Jacket",
         "images": [
-            "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-01.jpg",
-            "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-02.jpg",
+            "https://tailwindui.com/img/ecommerce-images/product-page-01-featured-product-shot.jpg",
+            "https://tailwindui.com/img/ecommerce-images/product-page-01-product-shot-01.jpg",
+            "https://tailwindui.com/img/ecommerce-images/product-page-01-product-shot-02.jpg",
         ],
-        "price": 199.99,
+        "colors": ["gray", "black"],
+        "sizes": ["xxs", "xs", "s", "m", "l", "xl"],
+        "description": """
+        The Basic tee is an honest new take on a classic. The tee uses super soft,
+        pre-shrunk cotton for true comfort and a dependable fit. They are hand cut and sewn locally,
+        with a special dye technique that gives each tee it's own look. Looking to stock your closet?
+        The Basic tee also comes in a 3-pack or 5-pack at a bundle discount.
+        """,
+        "rating": 3.9,
+        "additional_information": [
+            {
+                "title": "Features",
+                "text": [
+                    "Multiple strap configurations",
+                    "Spacious interior with top zip",
+                    "Leather handle and tabs",
+                    "Interior dividers",
+                    "Stainless strap loops",
+                    "Double stitched construction",
+                    "Water-resistant",
+                ],
+            }
+        ],
+        "price": 199999.99,
     },
     {
-        "_id": "5",
+        "id": "5",
         "name": "Silk Scarf",
         "images": [
-            "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-01.jpg",
-            "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-02.jpg",
+            "https://tailwindui.com/img/ecommerce-images/product-page-01-featured-product-shot.jpg",
+            "https://tailwindui.com/img/ecommerce-images/product-page-01-product-shot-01.jpg",
+            "https://tailwindui.com/img/ecommerce-images/product-page-01-product-shot-02.jpg",
         ],
-        "price": 39.99,
+        "colors": ["gray", "blue"],
+        "sizes": ["s", "m", "l", "xl"],
+        "description": """
+        The Basic tee is an honest new take on a classic. The tee uses super soft,
+        pre-shrunk cotton for true comfort and a dependable fit. They are hand cut and sewn locally,
+        with a special dye technique that gives each tee it's own look. Looking to stock your closet?
+        The Basic tee also comes in a 3-pack or 5-pack at a bundle discount.
+        """,
+        "rating": 2.0,
+        "additional_information": [
+            {
+                "title": "Features",
+                "text": [
+                    "Multiple strap configurations",
+                    "Spacious interior with top zip",
+                    "Leather handle and tabs",
+                    "Interior dividers",
+                    "Stainless strap loops",
+                    "Double stitched construction",
+                    "Water-resistant",
+                ],
+            }
+        ],
+        "price": 39999.99,
     },
     {
-        "_id": "6",
+        "id": "6",
         "name": "Linen Dress",
         "images": [
-            "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-01.jpg",
-            "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-02.jpg",
+            "https://tailwindui.com/img/ecommerce-images/product-page-01-featured-product-shot.jpg",
+            "https://tailwindui.com/img/ecommerce-images/product-page-01-product-shot-01.jpg",
+            "https://tailwindui.com/img/ecommerce-images/product-page-01-product-shot-02.jpg",
         ],
-        "price": 79.99,
+        "colors": ["gray", "black"],
+        "sizes": ["xxs", "xs", "s", "m"],
+        "description": """
+        The Basic tee is an honest new take on a classic. The tee uses super soft,
+        pre-shrunk cotton for true comfort and a dependable fit. They are hand cut and sewn locally,
+        with a special dye technique that gives each tee it's own look. Looking to stock your closet?
+        The Basic tee also comes in a 3-pack or 5-pack at a bundle discount.
+        """,
+        "rating": 3.5,
+        "additional_information": [
+            {
+                "title": "Features",
+                "text": [
+                    "Multiple strap configurations",
+                    "Spacious interior with top zip",
+                    "Leather handle and tabs",
+                    "Interior dividers",
+                    "Stainless strap loops",
+                    "Double stitched construction",
+                    "Water-resistant",
+                ],
+            }
+        ],
+        "price": 79999.99,
     },
 ]
