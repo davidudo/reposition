02c74d508970469ef224958615b1a22f8231261c
David Udo <udodavid46.ud@gmail.com>
2024-01-14 13:01:10 +0100

    Render products listing

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
 
 from . import views
+from .views import (
+    ProductCreateView,
+    ProductDeleteView,
+    ProductDetailView,
+    ProductListView,
+    ProductUpdateView,
+)
 
 urlpatterns = [
-    path("", views.products, name="products"),
+    path("", ProductListView.as_view(), name="product-list"),
+    path("create/", ProductCreateView.as_view(), name="product-create"),
+    path("update/<str:pk>/", ProductUpdateView.as_view(), name="product-update"),
+    path("delete/<str:pk>/", ProductDeleteView.as_view(), name="product-delete"),
+    path("<str:pk>/", ProductDetailView.as_view(), name="product-detail"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,43 @@
 from django.shortcuts import render
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    TemplateView,
+    UpdateView,
+)
+from products import products
+from products.models import Product
 
 
-def products(request):
-    return render(request, "products/products.html", {})
+class ProductListView(ListView):
+    model = Product
+    template_name = "products/products.html"
+    context_object_name = "products"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["products"] = products.products
+        return context
+
+
+class ProductDetailView(TemplateView):
+    model = Product
+    template_name = "products/product_details.html"
+    context_object_name = "product"
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    template_name = "products/products.html"
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    template_name = "products/products.html"
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = "products/products.html"
