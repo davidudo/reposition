6f22909e4aea29fc3c703f21da9f4ab41ab21038
David Udo <udodavid46.ud@gmail.com>
2024-01-14 13:00:33 +0100

    Create mock product data

---
+++ products/products.py
@@ -0,0 +1,56 @@
+products = [
+    {
+        "_id": "1",
+        "name": "Cotton Blend T-Shirt",
+        "images": [
+            "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-01.jpg",
+            "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-02.jpg",
+        ],
+        "price": 19.99,
+    },
+    {
+        "_id": "2",
+        "name": "Denim Jeans",
+        "images": [
+            "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-01.jpg",
+            "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-02.jpg",
+        ],
+        "price": 59.99,
+    },
+    {
+        "_id": "3",
+        "name": "Cashmere Sweater",
+        "images": [
+            "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-01.jpg",
+            "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-02.jpg",
+        ],
+        "price": 129.99,
+    },
+    {
+        "_id": "4",
+        "name": "Leather Jacket",
+        "images": [
+            "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-01.jpg",
+            "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-02.jpg",
+        ],
+        "price": 199.99,
+    },
+    {
+        "_id": "5",
+        "name": "Silk Scarf",
+        "images": [
+            "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-01.jpg",
+            "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-02.jpg",
+        ],
+        "price": 39.99,
+    },
+    {
+        "_id": "6",
+        "name": "Linen Dress",
+        "images": [
+            "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-01.jpg",
+            "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-02.jpg",
+        ],
+        "price": 79.99,
+    },
+]
